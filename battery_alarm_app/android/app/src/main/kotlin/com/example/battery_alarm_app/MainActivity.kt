ec84d5978a142eeb3b1801d880f1dcb3ef5b9dd4
Tim Otto <tim@timotto.io>
2024-05-17 20:45:26 +0200

    🫴 handle ble exceptions

--- battery_alarm_app/android/app/src/main/kotlin/com/example/battery_alarm_app/MainActivity.kt
+++ battery_alarm_app/android/app/src/main/kotlin/com/example/battery_alarm_app/MainActivity.kt
@@ -1,5 +1,22 @@
 package com.example.battery_alarm_app
 
+import android.os.Bundle
+import android.util.Log
+import com.polidea.rxandroidble2.exceptions.BleException
 import io.flutter.embedding.android.FlutterActivity
+import io.reactivex.exceptions.UndeliverableException
+import io.reactivex.plugins.RxJavaPlugins
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        RxJavaPlugins.setErrorHandler {
+            if (it is UndeliverableException && it.cause is BleException) {
+                Log.v("Battalarm", "Suppressed UndeliverableException: $it")
+            } else {
+                throw RuntimeException("Unexpected Throwable in RxJavaPlugins error handler", it)
+            }
+        }
+    }
+}
