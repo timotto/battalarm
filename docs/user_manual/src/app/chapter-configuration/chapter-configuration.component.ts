c5c299dad0886128ab02f64d6269b2a2d052f520
Tim Otto <tim@timotto.io>
2024-05-23 19:19:11 +0200

    📷 add app screenshots to docs

--- docs/user_manual/src/app/chapter-configuration/chapter-configuration.component.ts
+++ docs/user_manual/src/app/chapter-configuration/chapter-configuration.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component, OnDestroy} from '@angular/core';
+import {ImageModule} from "primeng/image";
+import {MediaMatcher} from "@angular/cdk/layout";
 
 @Component({
   selector: 'app-chapter-configuration',
   standalone: true,
-  imports: [],
+    imports: [
+        ImageModule
+    ],
   templateUrl: './chapter-configuration.component.html',
   styleUrl: './chapter-configuration.component.scss'
 })
-export class ChapterConfigurationComponent {
+export class ChapterConfigurationComponent implements OnDestroy {
+  mobileQuery: MediaQueryList;
 
+  private readonly _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addEventListener('change', this._mobileQueryListener);
+  }
+
+  protected get imageClass(): string {
+    return this.mobileQuery.matches ? 'mobile-image' : 'float-left-image'
+  }
+
+  protected get imageWidth(): string {
+    return this.mobileQuery.matches ? '120' : '240'
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeEventListener('change', this._mobileQueryListener);
+  }
 }
