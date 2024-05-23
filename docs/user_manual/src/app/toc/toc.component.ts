9d15d82352632affc0dbf18438cc43f07815dbaf
Tim Otto <tim@timotto.io>
2024-05-23 21:13:09 +0200

    🔦 highlight active navigation item

--- docs/user_manual/src/app/toc/toc.component.ts
+++ docs/user_manual/src/app/toc/toc.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {MatListItem, MatNavList} from "@angular/material/list";
-import {RouterLink} from "@angular/router";
+import {ActivatedRoute, RouterLink, RouterLinkActive, UrlSegment} from "@angular/router";
 
 @Component({
   selector: 'app-toc',
@@ -9,9 +9,42 @@ import {RouterLink} from "@angular/router";
     MatListItem,
     MatNavList,
     RouterLink,
+    RouterLinkActive,
   ],
   templateUrl: './toc.component.html',
   styleUrl: './toc.component.scss'
 })
 export class TocComponent {
+  constructor(route: ActivatedRoute) {
+    route.url.subscribe(urlSegment => this.onRouteChange(urlSegment))
+  }
+
+  protected readonly routes: TocRoute[] = [
+    {
+      title: $localize `Introduction`,
+      link: '/introduction',
+    },
+    {
+      title: $localize `Operation`,
+      link: '/operation',
+    },
+    {
+      title: $localize `Installation`,
+      link: '/installation',
+    },
+    {
+      title: $localize `Configuration`,
+      link: '/configuration',
+    },
+  ]
+
+  private onRouteChange(urlSegment: UrlSegment[]) {
+    console.log('on route change', urlSegment)
+  }
+}
+
+interface TocRoute {
+  title: string
+  link: string
+  active?: boolean
 }
