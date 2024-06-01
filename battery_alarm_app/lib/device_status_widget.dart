5c275675501a990feb10829a114ab181c0522538
Tim Otto <tim@timotto.io>
2024-06-01 09:41:24 +0200

    🖼️ support landscape status page

--- battery_alarm_app/lib/device_status_widget.dart
+++ battery_alarm_app/lib/device_status_widget.dart
@@ -18,19 +18,52 @@ class DeviceStatusWidget extends StatelessWidget {
   final DeviceClient deviceClient;
   final bool expert;
 
+  bool _isWide(Size size) => size.width > size.height;
+
+  Widget _contentPortrait(
+          BuildContext context, AsyncSnapshot<DeviceStatus> status) =>
+      ListView(
+        children: [
+          InGarageTile(value: status.data?.inGarage),
+          ChargingTile(value: status.data?.charging),
+          EngineRunningTile(value: status.data?.engineRunning),
+          VBatTile(value: status.data?.vbat),
+          if (expert) VBatDeltaTile(value: status.data?.vbatDelta),
+          RssiTile(value: status.data?.rssi),
+        ],
+      );
+
+  Widget _contentLandscape(
+          BuildContext context, AsyncSnapshot<DeviceStatus> status) =>
+      Row(
+        children: [
+          Expanded(
+            child: ListView(
+              children: [
+                InGarageTile(value: status.data?.inGarage),
+                ChargingTile(value: status.data?.charging),
+                EngineRunningTile(value: status.data?.engineRunning),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                VBatTile(value: status.data?.vbat),
+                if (expert) VBatDeltaTile(value: status.data?.vbatDelta),
+                RssiTile(value: status.data?.rssi),
+              ],
+            ),
+          ),
+        ],
+      );
+
   @override
   Widget build(BuildContext context) => StreamBuilder<DeviceStatus>(
         stream: deviceClient.statusService.deviceStatus,
         initialData: deviceClient.statusService.deviceStatusSnapshot,
-        builder: (context, status) => ListView(
-          children: [
-            InGarageTile(value: status.data?.inGarage),
-            ChargingTile(value: status.data?.charging),
-            EngineRunningTile(value: status.data?.engineRunning),
-            VBatTile(value: status.data?.vbat),
-            if (expert) VBatDeltaTile(value: status.data?.vbatDelta),
-            RssiTile(value: status.data?.rssi),
-          ],
-        ),
+        builder: (context, status) => _isWide(MediaQuery.sizeOf(context))
+            ? _contentLandscape(context, status)
+            : _contentPortrait(context, status),
       );
 }
