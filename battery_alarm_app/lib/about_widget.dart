b1566ee43b5756be7feba1a618278944ff7f2627
Tim Otto <tim@timotto.io>
2024-06-01 09:32:07 +0200

    💄 cosmetics

--- battery_alarm_app/lib/about_widget.dart
+++ battery_alarm_app/lib/about_widget.dart
@@ -1,4 +1,5 @@
 import 'package:battery_alarm_app/text.dart';
+import 'package:battery_alarm_app/widgets/app_icon.dart';
 import 'package:flutter/material.dart';
 
 class AboutWidget extends StatelessWidget {
@@ -9,11 +10,12 @@ class AboutWidget extends StatelessWidget {
       .toList(growable: false);
 
   @override
-  Widget build(BuildContext context) => Center(
-        child: ListView(
-          padding: const EdgeInsets.all(8),
-          children: _text(),
-        ),
+  Widget build(BuildContext context) => ListView(
+        padding: const EdgeInsets.all(8),
+        children: [
+          ..._text(),
+          const AppIcon(size: 192),
+        ],
       );
 }
 
